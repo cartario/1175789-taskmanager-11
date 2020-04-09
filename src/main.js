aa3f4ad5cc862f8a914c48b3413b63cf6a925617
Vasiliy <cartario@yandex.ru>
2020-04-09 21:51:26 +0300

    логика кнопки loadMoreButton показ/удалить, рефакторинг

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
-
-const TOTAL_TASKS = 3;
-
-// переношу куски разметки в js
 import {createSiteMenuTemplate} from "./components/SiteMenuTemplate.js";
 import {createSiteFilterTemplate} from "./components/SiteFilterTemplate.js";
 import {createBoardTemplate} from "./components/BoardTemplate.js";
 import {createEditCardTemplate} from "./components/EditCardTemplate.js";
 import {createTaskCardTemplate} from "./components/TaskCardTemplate.js";
+import {createloadMoreButtonTemplate} from "./components/loadMoreButtonTemplate.js";
 import {generateFilters} from "./mock/filter.js";
 import {generateTasks} from "./mock/task.js";
+
+const TOTAL_TASKS = 23;
+const SHOWING_TASKS_ON_START = 8;
+const SHOWING_TASKS_BY_BUTTON = 4;
 
 // ф-я отрисовки в доме
 const render = (container, template, place) => {
@@ -26,18 +27,39 @@ render(mainControl, createBoardTemplate(), `afterend`);
 render(mainControl, createSiteFilterTemplate(filters), `afterend`);
 render(mainControl, createSiteMenuTemplate(), `beforeend`);
 
+// находит ключевые узлы
 const boardTasks = main.querySelector(`.board__tasks`);
 
-
+// отрисовывает taskEdit
 render(boardTasks, createEditCardTemplate(tasks[0]), `afterbegin`);
 
-const renderTasks = () => {
-  for (let i = 1; i <= tasks.length - 1; i++) {
-    render(boardTasks, createTaskCardTemplate(tasks[i]), `beforeend`);
+let showingTasksCount = SHOWING_TASKS_ON_START;
 
-  }
+const renderTasksOnStart = () => {
+  tasks.slice(1, SHOWING_TASKS_ON_START).forEach((task) => {
+    render(boardTasks, createTaskCardTemplate(task), `beforeend`);
+  });
 };
 
-renderTasks();
+// отрисовывает tasks
+renderTasksOnStart();
 
+// отрисовывает loadMore
+render(boardTasks, createloadMoreButtonTemplate(), `afterend`);
 
+// находит клчевой узел после отрисовки
+const loadMoreButtonElement = document.querySelector(`.load-more`);
+
+// обработчик loadMore
+loadMoreButtonElement.addEventListener(`click`, () => {
+  const prevTasksCount = showingTasksCount;
+  showingTasksCount = showingTasksCount + SHOWING_TASKS_BY_BUTTON;
+
+  tasks.slice(prevTasksCount, showingTasksCount).forEach((task) => {
+    render(boardTasks, createTaskCardTemplate(task), `beforeend`);
+  });
+
+  if (showingTasksCount >= tasks.length) {
+    loadMoreButtonElement.remove();
+  }
+});
